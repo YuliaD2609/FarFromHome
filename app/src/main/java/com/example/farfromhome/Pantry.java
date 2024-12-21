d041d4407ed3cc152e4cbf169e656fc17563d852
your-id <y.dashchuk@studenti.unisa.it>
2024-12-21 01:11:54 +0100

    shopping list

---
+++ app/src/main/java/com/example/farfromhome/Pantry.java
@@ -0,0 +1,15 @@
+package com.example.farfromhome;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Pantry extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pantry_layout);
+        TextView titolo= findViewById(R.id.title);
+        titolo.setText("Dispensa");
+    }
+}
