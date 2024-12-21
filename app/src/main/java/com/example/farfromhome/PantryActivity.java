8165d30a469ba7dfbe8ede1725fde05ac9cd8394
your-id <y.dashchuk@studenti.unisa.it>
2024-12-21 11:53:56 +0100

    correzione errori

--- app/src/main/java/com/example/farfromhome/PantryActivity.java
+++ app/src/main/java/com/example/farfromhome/PantryActivity.java
@@ -2,11 +2,14 @@ package com.example.farfromhome;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PantryActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.pantry_layout); // Layout per la sezione Dispensa
+        setContentView(R.layout.pantry_layout);
+        TextView titolo= findViewById(R.id.title);
+        titolo.setText("Dispensa");
     }
 }
--- app/src/main/java/com/example/farfromhome/ShoppingListActivity.java
+++ app/src/main/java/com/example/farfromhome/ShoppingListActivity.java
@@ -2,11 +2,14 @@ package com.example.farfromhome;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ShoppingListActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.shopping_list_layout); // Layout per la sezione Lista della spesa
+        setContentView(R.layout.shopping_list_layout);
+        TextView titolo= findViewById(R.id.title);
+        titolo.setText("Lista della spesa");
     }
 }
